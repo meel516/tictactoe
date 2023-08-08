ee7f7af36153db90dc74da15493cd867a9aa5259
meel516 <md.saleem516@gmail.com>
2023-08-08 12:58:39 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Welcome</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/home/chat">Chat</a>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+     {/* <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className='container-fluid'>
+      <a class="navbar-brand" href="#">Welcome</a>
       <button className='navbar-toggler' type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label='Toggle navigation'>
         <span className='navbar-toggler-icon'></span>
       </button>
@@ -23,7 +45,8 @@ function App() {
        </li>
         </ul>
         </div>
-      </nav>
+        </div>
+      </nav> */}
     <div className="d-flex flex-column flex-sm-row justify-content-center">
      
        
