1bc005152f03336e299fd5fa283581de6edc7840
meel516 <md.saleem516@gmail.com>
2023-08-08 17:22:06 +0530

    ok

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -8,16 +8,19 @@ const ChatScreen = () => {
   const [userName,setuserName]=useState('')
   const [send,setSend]=useState('')
   const [refresh,Setrefresh]=useState(false)
-  const [checkmssg,setcheckmssg]=useState(false)
-  setTimeout(()=>setcheckmssg(prev=>!prev),1000)
   console.log('refreshes')
   useEffect(()=>{async function get(){
     const data= await axios.get('https://tictactoe-zsyj.onrender.com/getMessage')
     setMessages(data.data)
+  
     console.log(data)
 
   }
-get()},[refresh,checkmssg])
+get()
+const intervalId=setInterval(()=>{
+      get()
+},1000)
+return ()=>clearInterval(intervalId)},[refresh])
   return (
     <div className="chat-screen">
       {
@@ -57,15 +60,18 @@ get()},[refresh,checkmssg])
       </div>
       <div className="type-here-parent">
         <textarea className="type-here" placeholder="Type here..." onBlur={(e)=>setSend(e.target.value)}/>
-        <button className="send" onClick={async()=>{
+        <button type="button" onClick={async()=>{
  const confirmation = await axios.post('https://tictactoe-zsyj.onrender.com/newMessage',{username:userName,message:send
 
 })
 Setrefresh(prev=>!prev)
 setSend('')
-        }}>
-          <img className="more-square-icon" alt="" src="/moresquare.svg" />
-        </button>
+        }} class="btn btn-primary send">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"></path>
+</svg>
+              </button>
+      
       </div>
       </>}
     </div>
